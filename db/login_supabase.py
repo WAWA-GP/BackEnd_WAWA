bba4784bdb258e0d13b5c139711b77a146557544
songhyoeun <166578433+songhyoeun@users.noreply.github.com>
2025-09-26 00:19:59 +0900

    supabase 연결

--- db/login_supabase.py
+++ db/login_supabase.py
@@ -1,13 +1,13 @@
 import os
-from supabase import create_client, Client
+from supabase import create_async_client, AsyncClient
 from dotenv import load_dotenv
 
 load_dotenv()
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
 
-async def get_supabase_client() -> Client:
-    supabase: Client = create_client(url, key)
+# 👇 [수정] get_supabase_client 함수를 비동기 클라이언트를 생성하고 반환하도록 변경합니다.
+async def get_supabase_client() -> AsyncClient:
+    supabase: AsyncClient = await create_async_client(url, key)
     return supabase
