9bdd5f274469d2a537ea1af232af020147cbd381
songhyoeun <166578433+songhyoeun@users.noreply.github.com>
2025-09-26 00:17:15 +0900

    supabase 연결

--- core/database.py
+++ core/database.py
@@ -1,22 +1,17 @@
-# 데이터베이스 연결 및 세션 관리를 담당하는 파일
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+# core/database.py
 
-# 사용할 데이터베이스 주소를 설정합니다.
-DATABASE_URL = "sqlite:///./app.db"
+import os
+from dotenv import load_dotenv
+from supabase import create_async_client, AsyncClient
 
-# SQLAlchemy 엔진을 생성합니다.
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+load_dotenv()
 
-# 데이터베이스 세션 생성을 위한 클래스(SessionLocal)를 만듭니다.
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
 
-# API 엔드포인트에서 데이터베이스 세션을 사용하기 위한 의존성 함수입니다.
-def get_db():
-    db: Session = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+async def get_db() -> AsyncClient:
+    """
+    API 엔드포인트에서 사용할 Supabase 비동기 클라이언트를 생성하고 반환하는 의존성 함수입니다.
+    """
+    supabase: AsyncClient = await create_async_client(url, key)
+    return supabase
